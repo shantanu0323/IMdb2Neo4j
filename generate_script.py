53aaecb8c97e9562285a85cee1f33e1fea3b58c3
Anshul Hedau <anshulhedau01@gmail.com>
2017-11-03 00:57:54 +0530

    Update Actors file

--- generate_script.py
+++ generate_script.py
@@ -2,6 +2,14 @@ f = open("./output.json")
 s= f.read()
 import json
 root = json.loads(s)
+file = open("./script1.txt", "w")
+
 movies = root['Top250Movies']
+
 for movie in movies:
-    print(movie['title'])
+
+    # print("CREATE (RMD:TEAM {title:'Real Madrid', stadium:'Santiago Bernabeu'})")
+
+    for actor in movie['actors']:
+
+        file.write("MERGE (:MOVIE {title:'" + str(actor['name']).replace("'","-") + "'})\n")
