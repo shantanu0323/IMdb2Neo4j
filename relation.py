3574ca58243028622b7691d860eeb4c975605e74
Shantanu Pramanik <srs.shaan@gmail.com>
2017-11-05 21:53:46 +0530

    Final commit

--- relation.py
+++ relation.py
@@ -1,6 +1,7 @@
 f = open("./cast.json")
-s= f.read()
+s = f.read()
 import json
+
 root = json.loads(s)
 file = open("./script2.txt", "w")
 
@@ -11,8 +12,12 @@ for movie in movies:
     # print("CREATE (RMD:TEAM {title:'Real Madrid', stadium:'Santiago Bernabeu'})")
 
     for actor in movie['actors']:
-        #file.write("MERGE (:ACTOR {title:'" + str(actor['name']).replace("'","-") + "'})\n")
-        print("MATCH (actor:ACTOR {title:'"+  str(actor['name']).replace("'","-") + "'}),(movie:MOVIE {title:'" + str(movie['title']).replace("'","-") + "'})")
+        file.write(
+            "MATCH (actor:ACTOR {title:'" + str(actor['name']).replace("'", "-") + "'}),(movie:MOVIE {title:'" + str(
+                movie['title']).replace("'", "-") + "'})\n")
+        file.write("MERGE (actor)-[r:ACTED_IN]->(movie)\nUNION\n")
+        print("MATCH (actor:ACTOR {title:'" + str(actor['name']).replace("'", "-") + "'}),(movie:MOVIE {title:'" + str(
+            movie['title']).replace("'", "-") + "'})")
         print("MERGE (actor)-[r:ACTED_IN]->(movie)")
 # MATCH (charlie:Person { name: 'Charlie Sheen' }),(wallStreet:Movie { title: 'Wall Street' })
 # MERGE (charlie)-[r:ACTED_IN]->(wallStreet)
